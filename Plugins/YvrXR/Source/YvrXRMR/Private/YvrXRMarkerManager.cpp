8c21f92147c0d1463b82484141ce6a7da2048cf2
zhangmingyuan <zhangmingyuan@pfdm.cn>
2025-04-17 15:39:00 +0800

    fix: cannot receieve marker event
    [Module]:
    DetailInfo:
    
    Change-Id: I0004bb6af0cbd27bb0b8b5dbcd2a1a3fb25ce9e9

--- Plugins/YvrXR/Source/YvrXRMR/Private/YvrXRMarkerManager.cpp
+++ Plugins/YvrXR/Source/YvrXRMR/Private/YvrXRMarkerManager.cpp
@@ -56,7 +56,8 @@ void FYvrXRMarkerManager::PollEvent(XrEventDataBuffer EventData)
             XrSpaceLocation location{ XR_TYPE_SPACE_LOCATION };
 
             XR_ENSURE(xrLocateSpace(markers[id].space, YvrXRHMD->GetTrackingSpace(), YvrXRHMD->GetDisplayTime(), &location));
-            if (location.locationFlags & XR_SPACE_LOCATION_POSITION_TRACKED_BIT)
+            // Fixme
+            // if (location.locationFlags & XR_SPACE_LOCATION_POSITION_TRACKED_BIT)
             {
                 FTransform markerTransform{};
                 const XrPosef& pose = location.pose;
